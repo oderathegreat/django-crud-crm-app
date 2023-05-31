c3ceb9cbdd8d38bb7e0c295d2fef7b0cca3b989d
oderathegreat <oderathegreat@gmail.com>
2023-05-31 16:21:05 +0300

    added rest framework and serializer

--- crm_app/admin.py
+++ crm_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Payment
+from .models import Trans
 
 admin.site.register(Payment)
+admin.site.register(Trans)
--- crm_app/models.py
+++ crm_app/models.py
@@ -13,3 +13,9 @@ class Payment(models.Model):
           return f"{self.first_name} {self.last_name}"
 
 
+class Trans(models.Model):
+       amount = models.CharField(max_length=50)
+       transcode = models.CharField(max_length=100)
+
+       def __str__(self):
+            return f"{self.amount} {self.transcode}"
